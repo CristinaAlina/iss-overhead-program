c012c245e88222901bbef66576f537e44ac98eb9
CristinaAlina <sirbu.cristina.alina94@gmail.com>
2024-01-12 01:34:29 +0200

    Created a function that returns if it's night or not, using API for getting sunrise and sunset times and comparing the hour of both with current time hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 MY_LATITUDE = 51.507351  # Your latitude
 MY_LONGITUDE = -0.127758  # Your longitude
@@ -17,3 +18,20 @@ def is_overhead():
         return True
     else:
         return False
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LATITUDE,
+        "lng": MY_LONGITUDE,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    return (sunset <= time_now.hour < 24) or time_now.hour == 0 or (0 < time_now.hour <= sunrise)
